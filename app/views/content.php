248815cefada35bdf60aab19aedb356b15a5a9a0
Dikodikij <53814520+Dikodikij@users.noreply.github.com>
2020-12-14 13:41:39 +0200

    tinklapis Rūtai
    galutinių failų įkėlimas

---
+++ app/views/content.php
@@ -0,0 +1,20 @@
+    <!-- Contact -->
+    
+    <section class="contact">
+        <div class="container">
+            <div class="section-heading">
+                <h2></h2>
+            </div>
+            <div class="section-content">
+                <form class="contact-form" action="index.php" method="post">
+                    <div class="input-row">
+                        <input name="vardas" type="text" placeholder="Jūsų Vardas" required autofocus>
+                        <input name="email" type="email"  placeholder="Jūsų El. Paštas" required>
+                    </div>
+                    <textarea name="message" rows="8" placeholder="Jūsų žinutė" required></textarea>
+                    <input class="btn btn-form" type="submit" name="submit">
+                </form>
+            </div>
+        
+        </div>
+    </section>
---
+++ app/views/header.php
@@ -0,0 +1,22 @@
+<head>
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;1,300&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/5146ff8444.js" crossorigin="anonymous"></script>
+</head>
+
+<header class="site-header">
+        <div class="container flex-container">
+            <div class="logo">
+                <a href="../index.html"><img src="../images/logo_pirmas_didesnis2.png" alt="Medinet Logo"></a>
+            </div>
+            <nav class="main-nav">
+                <ul class="flex-container">
+                    <li><a href="../index.html">NAUJIENOS</a></li>
+                    <li><a href="#">PREKĖS</a></li>
+                    <li><a href="#">SUSISIEKIME</a></li>
+                    <li><a href="#">KAIP MUS RASTI</a></li>
+                    <li><a href="tel:+370 680 31300"><i class="fa fa-phone"></i> +370 680 31300</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
